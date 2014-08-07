134a4fd0c3eeeba2f1f98975d470f6d1fa0961e5
Tuan Nguyen <amtuannguyen@gmail.com>
2014-08-07 00:19:40 -0400

    search for error in import logs

--- reindex.sh
+++ reindex.sh
@@ -30,14 +30,23 @@ cd $VUFIND_HOME || exit
 
 # import catalog marc files
 [ -f /tmp/catalog.mrc ] && /usr/local/vufind/import-marc.sh /tmp/catalog.mrc &>/tmp/import-catalog.log
+grep -A2 -B2 -i exception /tmp/import-catalog.log
+grep -A2 -B2 -i error /tmp/import-catalog.log
+grep -A2 -B2 -i severe /tmp/import-catalog.log
 
 # import SFX journals marcxml
 [ -f /tmp/sfx-journals.xml ] && curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>data_source_str:SFX</query></delete>'
 [ -f /tmp/sfx-journals.xml ] && /usr/local/vufind/import-sfx.sh /tmp/sfx-journals.xml &>/tmp/import-sfx-journals.log
+grep -A2 -B2 -i exception /tmp/import-sfx-journals.log
+grep -A2 -B2 -i error /tmp/import-sfx-journals.log
+grep -A2 -B2 -i severe /tmp/import-sfx-journals.log
 
 # import MULER marc 
 [ -f /tmp/muler.mrc ] && curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>data_source_str:MULER</query></delete>'
 [ -f /tmp/muler.mrc ] && /usr/local/vufind/import-muler.sh /tmp/muler.mrc &>/tmp/import-muler.log
+grep -A2 -B2 -i exception /tmp/import-muler.log
+grep -A2 -B2 -i error /tmp/import-muler.log
+grep -A2 -B2 -i severe /tmp/import-muler.log
 
 # cleanup and commit
 curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>format:Delete</query></delete>'
