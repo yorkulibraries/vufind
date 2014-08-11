06b3ef51ccc9db0dd14c025653f566c0cb54cb29
Tuan Nguyen <amtuannguyen@gmail.com>
2014-08-11 15:35:54 -0400

    send curl STDERR output to /dev/null

--- reindex.sh
+++ reindex.sh
@@ -35,23 +35,23 @@ grep -A2 -B2 -i error /tmp/import-catalog.log
 grep -A2 -B2 -i severe /tmp/import-catalog.log
 
 # import SFX journals marcxml
-[ -f /tmp/sfx-journals.xml ] && curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>data_source_str:SFX</query></delete>'
+[ -f /tmp/sfx-journals.xml ] && curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>data_source_str:SFX</query></delete>' 1>/dev/null
 [ -f /tmp/sfx-journals.xml ] && /usr/local/vufind/import-sfx.sh /tmp/sfx-journals.xml &>/tmp/import-sfx-journals.log
 grep -A2 -B2 -i exception /tmp/import-sfx-journals.log
 grep -A2 -B2 -i error /tmp/import-sfx-journals.log
 grep -A2 -B2 -i severe /tmp/import-sfx-journals.log
 
 # import MULER marc 
-[ -f /tmp/muler.mrc ] && curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>data_source_str:MULER</query></delete>'
+[ -f /tmp/muler.mrc ] && curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>data_source_str:MULER</query></delete>' 1>/dev/null
 [ -f /tmp/muler.mrc ] && /usr/local/vufind/import-muler.sh /tmp/muler.mrc &>/tmp/import-muler.log
 grep -A2 -B2 -i exception /tmp/import-muler.log
 grep -A2 -B2 -i error /tmp/import-muler.log
 grep -A2 -B2 -i severe /tmp/import-muler.log
 
 # cleanup and commit
-curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>format:Delete</query></delete>'
-curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>title:"**REQUIRED FIELD**"</query></delete>'
-curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<commit/>'
+curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>format:Delete</query></delete>' 1>/dev/null
+curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>title:"**REQUIRED FIELD**"</query></delete>' 1>/dev/null
+curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<commit/>' 1>/dev/null
 
 # shutdown and restart
 /usr/local/vufind/vufind.sh stop
