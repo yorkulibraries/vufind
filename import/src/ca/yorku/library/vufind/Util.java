29091f1644d14f09883b81fdd79f44e5b6bc251b
Tuan Nguyen <amtuannguyen@gmail.com>
2014-08-18 14:18:41 -0400

    created ant build.xml replacing jardesc

--- import/src/ca/yorku/library/vufind/Util.java
+++ import/src/ca/yorku/library/vufind/Util.java
@@ -1,9 +1,17 @@
 package ca.yorku.library.vufind;
 
+import org.marc4j.*;
+import org.marc4j.marc.*;
+import java.io.*;
+import java.sql.*;
+
+import org.apache.log4j.*;
+
 public class Util {
 
 	public static void main(String[] args) {
-		System.err.println("Hello");
+		Logger logger = Logger.getLogger(ca.yorku.library.vufind.Util.class);
+		logger.info("Hello, world");
 	}
 
 }
