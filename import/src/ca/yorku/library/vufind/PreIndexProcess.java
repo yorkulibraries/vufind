18b6c633620a0c305c7122aea4699a9154688981
Tuan Nguyen <amtuannguyen@gmail.com>
2014-08-20 10:45:18 -0400

    use spaces for indentation

--- import/src/ca/yorku/library/vufind/PreIndexProcess.java
+++ import/src/ca/yorku/library/vufind/PreIndexProcess.java
@@ -16,155 +16,156 @@ import org.marc4j.marc.Record;
 import org.solrmarc.tools.SolrMarcIndexerException;
 
 public class PreIndexProcess implements Runnable {
-	private String recordId = null;
-	private String source = null;
-	private String file = null;
-	private Connection db = null;
-	private PreparedStatement saveResolverIdStmt = null;
-	private PreparedStatement saveISSNStmt = null;
-	private PreparedStatement saveMARCStmt = null;
+    private String recordId = null;
+    private String source = null;
+    private String file = null;
+    private Connection db = null;
+    private PreparedStatement saveResolverIdStmt = null;
+    private PreparedStatement saveISSNStmt = null;
+    private PreparedStatement saveMARCStmt = null;
 
-	// Initialize logging category
-	static Logger logger = Logger.getLogger(PreIndexProcess.class.getName());
+    // Initialize logging category
+    static Logger logger = Logger.getLogger(PreIndexProcess.class.getName());
 
-	static String catalog = "/tmp/catalog.mrc";
-	static String muler = "/tmp/muler.mrc";
-	static String sfxJournals = "/tmp/sfx-journals.xml";
-	static String[] resolverPrefixes = {
-		"http://www.library.yorku.ca/eresolver/?id=",
-		"http://www.library.yorku.ca/e/resolver/id/" 
-	};
-	static String issnFieldSpecs = "022a:022y:440x:490x:730x:776x:780x:785x";
-	
-	static String insertResolverIdSql = 
-		"insert into resolver_ids (record_id, number, source) "
-		+ "values (?, ?, ?) on duplicate key update id=id";
-	
-	static String insertISSNSql = 
-			"insert into issns (record_id, number, source) "
-			+ "values (?, ?, ?) on duplicate key update id=id";
-	
-	static String insertMARCSql = 
-			"insert into marc (record_id, marc) "
-			+ "values (?, ?) on duplicate key update id=id";
-	
-	public PreIndexProcess(String file, String source, Connection db) throws SQLException {
-		this.file = file;
-		this.source = source;
-		this.db = db;
-		
-		this.saveResolverIdStmt = db.prepareStatement(insertResolverIdSql);
-		this.saveISSNStmt = db.prepareStatement(insertISSNSql);
-		this.saveMARCStmt = db.prepareStatement(insertMARCSql);
-	}
+    static String catalog = "/tmp/catalog.mrc";
+    static String muler = "/tmp/muler.mrc";
+    static String sfxJournals = "/tmp/sfx-journals.xml";
+    static String[] resolverPrefixes = {
+            "http://www.library.yorku.ca/eresolver/?id=",
+            "http://www.library.yorku.ca/e/resolver/id/" };
+    static String issnFieldSpecs = "022a:022y:440x:490x:730x:776x:780x:785x";
 
-	public static void main(String[] args) throws Exception {		
-		String dsn = Utils.getConfigSetting("config.ini", "Database",
-				"database");
-		Connection db = null;
-		try {
-			db = Utils.connectToDatabase(dsn);
-		} catch (Exception e) {
-			throw new SolrMarcIndexerException(SolrMarcIndexerException.EXIT,
-					e.getMessage());
-		}
+    static String insertResolverIdSql = "insert into resolver_ids (record_id, number, source) "
+            + "values (?, ?, ?) on duplicate key update id=id";
 
-		Thread t = new Thread(new PreIndexProcess(catalog, "sirsi", db));
-		t.start();
+    static String insertISSNSql = "insert into issns (record_id, number, source) "
+            + "values (?, ?, ?) on duplicate key update id=id";
 
-		Thread t2 = new Thread(new PreIndexProcess(muler, "muler", db));
-		t2.start();
+    static String insertMARCSql = "insert into marc (record_id, marc) "
+            + "values (?, ?) on duplicate key update id=id";
 
-		Thread t3 = new Thread(new PreIndexProcess(sfxJournals, "sfx", db));
-		t3.start();
-	}
+    public PreIndexProcess(String file, String source, Connection db)
+            throws SQLException {
+        this.file = file;
+        this.source = source;
+        this.db = db;
 
-	private void process(Record record) throws SQLException {
-		recordId = Utils.getRecordId(record, source);
-		if (source.equals("sirsi")) {
-			saveResolverIDs(record);
-			saveMARC(record);
-		}
-		saveISSNs(record);
-	}
+        this.saveResolverIdStmt = db.prepareStatement(insertResolverIdSql);
+        this.saveISSNStmt = db.prepareStatement(insertISSNSql);
+        this.saveMARCStmt = db.prepareStatement(insertMARCSql);
+    }
 
-	private void saveResolverIDs(Record record) throws SQLException {
-		if (recordId != null) {
-			List<String> urls = Utils.getFieldValues(record, "856u:856a");
-			for (String url : urls) {
-				for (String prefix : resolverPrefixes) {
-					if (url.startsWith(prefix)) {
-						String resolverId = url.substring(prefix.length());
-						logger.debug("resolver ID number: " + resolverId + " for record ID: " + recordId);
-						if (resolverId.matches("[0-9]+")) {
-							saveResolverId(recordId, resolverId);
-						}
-					}
-				}
-			}
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        String dsn = Utils.getConfigSetting("config.ini", "Database",
+                "database");
+        Connection db = null;
+        try {
+            db = Utils.connectToDatabase(dsn);
+        } catch (Exception e) {
+            throw new SolrMarcIndexerException(SolrMarcIndexerException.EXIT,
+                    e.getMessage());
+        }
 
-	private void saveISSNs(Record record) throws SQLException {
-		if (recordId != null) {
-			List<String> issns = Utils.getFieldValues(record, issnFieldSpecs);
-			for (String issn : issns) {
-				issn = issn.toUpperCase().replaceAll("[^0-9X]", "");
-				logger.debug("ISSN number: " + issn + " for record ID: " + recordId);
-				if (issn.matches("[0-9X]{8}")) {
-					saveISSN(recordId, issn);
-				}
-			}
-		}
-	}
-	
-	private void saveResolverId(String recordId, String resolverId) throws SQLException {
-		saveResolverIdStmt.setString(1, recordId);
-		saveResolverIdStmt.setString(2, resolverId);
-		saveResolverIdStmt.setString(3, source);
-		saveResolverIdStmt.execute();
-	}
-	
-	private void saveISSN(String recordId, String issn) throws SQLException {
-		saveISSNStmt.setString(1, recordId);
-		saveISSNStmt.setString(2, issn);
-		saveISSNStmt.setString(3, source);
-		saveISSNStmt.execute();
-	}
-	
-	private void saveMARC(Record record) {
-		if (recordId != null) {
-			try {
-				String marc = Utils.writeRaw(record);
-				saveMARCStmt.setString(1, recordId);
-				saveMARCStmt.setString(2, marc);
-				saveMARCStmt.execute();
-			} catch (Exception e) {
-				// this step is not very critical, logging the error is enough
-				logger.error(e.getMessage());
-			}
-		}
-	}
+        Thread t = new Thread(new PreIndexProcess(catalog, "sirsi", db));
+        t.start();
 
-	@Override
-	public void run() {
-		logger.info("Processing source=" + source + ", file=" + file);
-		int count = 0;
-		try {
-			File f = new File(file);
-			if (f.exists()) {
-				InputStream in = new FileInputStream(f);
-				MarcReader reader = (source.equals("sfx")) ? new MarcXmlReader(
-						in) : new MarcStreamReader(in);
-				while (reader.hasNext()) {
-					Record record = reader.next();
-					process(record);
-					count++;
-				}
-			}
-		} catch (Exception e) {
-			logger.error(e.getMessage());
-		}
-		logger.info("Processed " + count + " records. source=" + source + ", file=" + file);
-	}
+        Thread t2 = new Thread(new PreIndexProcess(muler, "muler", db));
+        t2.start();
+
+        Thread t3 = new Thread(new PreIndexProcess(sfxJournals, "sfx", db));
+        t3.start();
+    }
+
+    private void process(Record record) throws SQLException {
+        recordId = Utils.getRecordId(record, source);
+        if (source.equals("sirsi")) {
+            saveResolverIDs(record);
+            saveMARC(record);
+        }
+        saveISSNs(record);
+    }
+
+    private void saveResolverIDs(Record record) throws SQLException {
+        if (recordId != null) {
+            List<String> urls = Utils.getFieldValues(record, "856u:856a");
+            for (String url : urls) {
+                for (String prefix : resolverPrefixes) {
+                    if (url.startsWith(prefix)) {
+                        String resolverId = url.substring(prefix.length());
+                        logger.debug("resolver ID number: " + resolverId
+                                + " for record ID: " + recordId);
+                        if (resolverId.matches("[0-9]+")) {
+                            saveResolverId(recordId, resolverId);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void saveISSNs(Record record) throws SQLException {
+        if (recordId != null) {
+            List<String> issns = Utils.getFieldValues(record, issnFieldSpecs);
+            for (String issn : issns) {
+                issn = issn.toUpperCase().replaceAll("[^0-9X]", "");
+                logger.debug("ISSN number: " + issn + " for record ID: "
+                        + recordId);
+                if (issn.matches("[0-9X]{8}")) {
+                    saveISSN(recordId, issn);
+                }
+            }
+        }
+    }
+
+    private void saveResolverId(String recordId, String resolverId)
+            throws SQLException {
+        saveResolverIdStmt.setString(1, recordId);
+        saveResolverIdStmt.setString(2, resolverId);
+        saveResolverIdStmt.setString(3, source);
+        saveResolverIdStmt.execute();
+    }
+
+    private void saveISSN(String recordId, String issn) throws SQLException {
+        saveISSNStmt.setString(1, recordId);
+        saveISSNStmt.setString(2, issn);
+        saveISSNStmt.setString(3, source);
+        saveISSNStmt.execute();
+    }
+
+    private void saveMARC(Record record) {
+        if (recordId != null) {
+            try {
+                String marc = Utils.writeRaw(record);
+                saveMARCStmt.setString(1, recordId);
+                saveMARCStmt.setString(2, marc);
+                saveMARCStmt.execute();
+            } catch (Exception e) {
+                // this step is not very critical, logging the error is enough
+                logger.error(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        logger.info("Processing source=" + source + ", file=" + file);
+        int count = 0;
+        try {
+            File f = new File(file);
+            if (f.exists()) {
+                InputStream in = new FileInputStream(f);
+                MarcReader reader = (source.equals("sfx")) ? new MarcXmlReader(
+                        in) : new MarcStreamReader(in);
+                while (reader.hasNext()) {
+                    Record record = reader.next();
+                    process(record);
+                    count++;
+                }
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        logger.info("Processed " + count + " records. source=" + source
+                + ", file=" + file);
+    }
 }
--- import/src/ca/yorku/library/vufind/Utils.java
+++ import/src/ca/yorku/library/vufind/Utils.java
@@ -24,243 +24,242 @@ import org.marc4j.marc.Subfield;
 import org.marc4j.marc.VariableField;
 
 public class Utils {
-	// Initialize logging category
-	static Logger logger = Logger.getLogger(Utils.class.getName());
+    // Initialize logging category
+    static Logger logger = Logger.getLogger(Utils.class.getName());
 
-	static String sirsiCatkeyPrefix = "(Sirsi) a";
-	
-	/**
-	 * Load an ini file.
-	 * 
-	 * @param filename
-	 * @throws IOException
-	 * @throws FileNotFoundException
-	 * @throws InvalidFileFormatException
-	 */
-	public static Ini loadConfigFile(String filename)
-			throws InvalidFileFormatException, FileNotFoundException,
-			IOException {
-		Ini ini = new Ini();
-		ini.load(new FileReader(findConfigFile(filename)));
-		return ini;
-	}
+    static String sirsiCatkeyPrefix = "(Sirsi) a";
 
-	/**
-	 * Connect to the VuFind database.
-	 * 
-	 * @throws ClassNotFoundException
-	 * @throws IllegalAccessException
-	 * @throws InstantiationException
-	 * @throws SQLException
-	 */
-	public static Connection connectToDatabase(String dsn)
-			throws InstantiationException, IllegalAccessException,
-			ClassNotFoundException, SQLException {
-		// Parse key settings from the PHP-style DSN:
-		String username = "";
-		String password = "";
-		String classname = "invalid";
-		String prefix = "invalid";
-		if (dsn.substring(0, 8).equals("mysql://")) {
-			classname = "com.mysql.jdbc.Driver";
-			prefix = "mysql";
-		} else if (dsn.substring(0, 8).equals("pgsql://")) {
-			classname = "org.postgresql.Driver";
-			prefix = "postgresql";
-		}
+    /**
+     * Load an ini file.
+     * 
+     * @param filename
+     * @throws IOException
+     * @throws FileNotFoundException
+     * @throws InvalidFileFormatException
+     */
+    public static Ini loadConfigFile(String filename)
+            throws InvalidFileFormatException, FileNotFoundException,
+            IOException {
+        Ini ini = new Ini();
+        ini.load(new FileReader(findConfigFile(filename)));
+        return ini;
+    }
 
-		Class.forName(classname).newInstance();
-		String[] parts = dsn.split("://");
-		if (parts.length > 1) {
-			parts = parts[1].split("@");
-			if (parts.length > 1) {
-				dsn = prefix + "://" + parts[1];
-				parts = parts[0].split(":");
-				username = parts[0];
-				if (parts.length > 1) {
-					password = parts[1];
-				}
-			}
-		}
+    /**
+     * Connect to the VuFind database.
+     * 
+     * @throws ClassNotFoundException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     * @throws SQLException
+     */
+    public static Connection connectToDatabase(String dsn)
+            throws InstantiationException, IllegalAccessException,
+            ClassNotFoundException, SQLException {
+        // Parse key settings from the PHP-style DSN:
+        String username = "";
+        String password = "";
+        String classname = "invalid";
+        String prefix = "invalid";
+        if (dsn.substring(0, 8).equals("mysql://")) {
+            classname = "com.mysql.jdbc.Driver";
+            prefix = "mysql";
+        } else if (dsn.substring(0, 8).equals("pgsql://")) {
+            classname = "org.postgresql.Driver";
+            prefix = "postgresql";
+        }
 
-		// Connect to the database:
-		return DriverManager.getConnection("jdbc:" + dsn, username, password);
-	}
+        Class.forName(classname).newInstance();
+        String[] parts = dsn.split("://");
+        if (parts.length > 1) {
+            parts = parts[1].split("@");
+            if (parts.length > 1) {
+                dsn = prefix + "://" + parts[1];
+                parts = parts[0].split(":");
+                username = parts[0];
+                if (parts.length > 1) {
+                    password = parts[1];
+                }
+            }
+        }
 
-	/**
-	 * Given the base name of a configuration file, locate the full path.
-	 * 
-	 * @param filename
-	 */
-	public static File findConfigFile(String filename) {
-		// Find VuFind's home directory in the environment; if it's not
-		// available,
-		// try using a relative path on the assumption that we are currently in
-		// VuFind's import subdirectory:
-		String vufindHome = System.getenv("VUFIND_HOME");
-		if (vufindHome == null) {
-			vufindHome = "..";
-		}
-		logger.debug("VUFIND_HOME=" + vufindHome);
+        // Connect to the database:
+        return DriverManager.getConnection("jdbc:" + dsn, username, password);
+    }
 
-		// Check for VuFind 2.0's local directory environment variable:
-		String vufindLocal = System.getenv("VUFIND_LOCAL_DIR");
-		logger.debug("VUFIND_LOCAL_DIR=" + vufindLocal);
+    /**
+     * Given the base name of a configuration file, locate the full path.
+     * 
+     * @param filename
+     */
+    public static File findConfigFile(String filename) {
+        // Find VuFind's home directory in the environment; if it's not
+        // available,
+        // try using a relative path on the assumption that we are currently in
+        // VuFind's import subdirectory:
+        String vufindHome = System.getenv("VUFIND_HOME");
+        if (vufindHome == null) {
+            vufindHome = "..";
+        }
+        logger.debug("VUFIND_HOME=" + vufindHome);
 
-		String[] propertyDirs = { vufindHome + "/import",
-				vufindHome + "/import/translation_maps",
-				vufindHome + "/import/index_scripts" };
-		Properties vuFindConfigs = org.solrmarc.tools.Utils.loadProperties(
-				propertyDirs, "vufind.properties");
+        // Check for VuFind 2.0's local directory environment variable:
+        String vufindLocal = System.getenv("VUFIND_LOCAL_DIR");
+        logger.debug("VUFIND_LOCAL_DIR=" + vufindLocal);
 
-		// Get the relative VuFind path from the properties file, defaulting to
-		// the 2.0alpha-style application/configs if necessary.
-		String relativeConfigPath = org.solrmarc.tools.Utils.getProperty(
-				vuFindConfigs, "vufind.config.relative_path",
-				"application/configs");
-		logger.debug("vufind.config.relative_path=" + relativeConfigPath);
+        String[] propertyDirs = { vufindHome + "/import",
+                vufindHome + "/import/translation_maps",
+                vufindHome + "/import/index_scripts" };
+        Properties vuFindConfigs = org.solrmarc.tools.Utils.loadProperties(
+                propertyDirs, "vufind.properties");
 
-		// Try several different locations for the file -- VuFind 2 local dir,
-		// VuFind 2 base dir, VuFind 1 base dir.
-		File file;
-		if (vufindLocal != null) {
-			file = new File(vufindLocal + "/" + relativeConfigPath + "/"
-					+ filename);
-			if (file.exists()) {
-				return file;
-			}
-		}
-		file = new File(vufindHome + "/" + relativeConfigPath + "/" + filename);
-		if (file.exists()) {
-			return file;
-		}
-		file = new File(vufindHome + "/web/conf/" + filename);
-		return file;
-	}
+        // Get the relative VuFind path from the properties file, defaulting to
+        // the 2.0alpha-style application/configs if necessary.
+        String relativeConfigPath = org.solrmarc.tools.Utils.getProperty(
+                vuFindConfigs, "vufind.config.relative_path",
+                "application/configs");
+        logger.debug("vufind.config.relative_path=" + relativeConfigPath);
 
-	/**
-	 * Sanitize a VuFind configuration setting.
-	 * 
-	 * @param str
-	 */
-	public static String sanitizeConfigSetting(String str) {
-		// Drop comments if necessary:
-		int pos = str.indexOf(';');
-		if (pos >= 0) {
-			str = str.substring(0, pos).trim();
-		}
+        // Try several different locations for the file -- VuFind 2 local dir,
+        // VuFind 2 base dir, VuFind 1 base dir.
+        File file;
+        if (vufindLocal != null) {
+            file = new File(vufindLocal + "/" + relativeConfigPath + "/"
+                    + filename);
+            if (file.exists()) {
+                return file;
+            }
+        }
+        file = new File(vufindHome + "/" + relativeConfigPath + "/" + filename);
+        if (file.exists()) {
+            return file;
+        }
+        file = new File(vufindHome + "/web/conf/" + filename);
+        return file;
+    }
 
-		// Strip wrapping quotes if necessary (the ini reader won't do this for
-		// us):
-		if (str.startsWith("\"")) {
-			str = str.substring(1, str.length());
-		}
-		if (str.endsWith("\"")) {
-			str = str.substring(0, str.length() - 1);
-		}
-		return str;
-	}
+    /**
+     * Sanitize a VuFind configuration setting.
+     * 
+     * @param str
+     */
+    public static String sanitizeConfigSetting(String str) {
+        // Drop comments if necessary:
+        int pos = str.indexOf(';');
+        if (pos >= 0) {
+            str = str.substring(0, pos).trim();
+        }
 
-	/**
-	 * Get a setting from a VuFind configuration file.
-	 * 
-	 * @param filename
-	 * @param section
-	 * @param setting
-	 * @throws IOException
-	 * @throws FileNotFoundException
-	 * @throws InvalidFileFormatException
-	 */
-	public static String getConfigSetting(String filename, String section,
-			String setting) throws InvalidFileFormatException,
-			FileNotFoundException, IOException {
-		String retVal = null;
+        // Strip wrapping quotes if necessary (the ini reader won't do this for
+        // us):
+        if (str.startsWith("\"")) {
+            str = str.substring(1, str.length());
+        }
+        if (str.endsWith("\"")) {
+            str = str.substring(0, str.length() - 1);
+        }
+        return str;
+    }
 
-		// Grab the ini file.
-		Ini ini = loadConfigFile(filename);
+    /**
+     * Get a setting from a VuFind configuration file.
+     * 
+     * @param filename
+     * @param section
+     * @param setting
+     * @throws IOException
+     * @throws FileNotFoundException
+     * @throws InvalidFileFormatException
+     */
+    public static String getConfigSetting(String filename, String section,
+            String setting) throws InvalidFileFormatException,
+            FileNotFoundException, IOException {
+        String retVal = null;
 
-		// Check to see if we need to worry about an override file:
-		String override = ini.get("Extra_Config", "local_overrides");
-		if (override != null) {
-			Ini overrideIni = loadConfigFile(override);
-			retVal = overrideIni.get(section, setting);
-			if (retVal != null) {
-				return sanitizeConfigSetting(retVal);
-			}
-		}
+        // Grab the ini file.
+        Ini ini = loadConfigFile(filename);
 
-		// Try to find the requested setting:
-		retVal = ini.get(section, setting);
+        // Check to see if we need to worry about an override file:
+        String override = ini.get("Extra_Config", "local_overrides");
+        if (override != null) {
+            Ini overrideIni = loadConfigFile(override);
+            retVal = overrideIni.get(section, setting);
+            if (retVal != null) {
+                return sanitizeConfigSetting(retVal);
+            }
+        }
 
-		// No setting? Check for a parent configuration:
-		if (retVal == null) {
-			String parent = ini.get("Parent_Config", "path");
-			if (parent != null) {
-				ini = loadConfigFile(parent);
-				retVal = ini.get(section, setting);
-			}
-		}
+        // Try to find the requested setting:
+        retVal = ini.get(section, setting);
 
-		// Return the processed setting:
-		return retVal == null ? null : sanitizeConfigSetting(retVal);
-	}
+        // No setting? Check for a parent configuration:
+        if (retVal == null) {
+            String parent = ini.get("Parent_Config", "path");
+            if (parent != null) {
+                ini = loadConfigFile(parent);
+                retVal = ini.get(section, setting);
+            }
+        }
 
-	public static List<String> getFieldValues(Record record, String fieldSpecs) {
-		List<String> results = new ArrayList<String>();
-		String[] specs = fieldSpecs.split(":");
-		for (String spec : specs) {
-			if (spec.length() == 4) {
-				String tag = spec.substring(0, 3);
-				char code = spec.charAt(3);
-				List<VariableField> fields = record.getVariableFields(tag);
-				for (VariableField f : fields) {
-					DataField field = (DataField) f;
-					Subfield subfield = field.getSubfield(code);
-					if (subfield != null) {
-						results.add(subfield.getData().trim());
-					}
-				}
-			}
-		}
-		return results;
-	}
-	
-	public static String getFirstFieldValue(Record record, String fieldSpecs) {
-		List<String> values = getFieldValues(record, fieldSpecs);
-		return values.isEmpty() ? null : values.get(0);
-	}
-	
-	public static String getRecordId(Record record, String source) {
-		String id = null;
-	    if ("sirsi".equals(source)) {
-	    	List<String> vals = getFieldValues(record, "035a");
-	    	for (String val : vals) {
-	    		if (val.startsWith(sirsiCatkeyPrefix)) {
-	    			id = val.substring(sirsiCatkeyPrefix.length());
-	    			break;
-	    		}
-	    	}
-	    } else if ("sfx".equals(source)) {
-	        id = getFirstFieldValue(record, "090a");
-	    } else if ("muler".equals(source)) {
-	        id = getFirstFieldValue(record, "035a");
-	    }
-	    return id;
-	}
-	
-	public static String writeRaw(Record record) {
-	    ByteArrayOutputStream out = new ByteArrayOutputStream();
-	    MarcWriter writer = new MarcStreamWriter(out, "UTF-8", true);
-	    writer.write(record);
-	    writer.close();
+        // Return the processed setting:
+        return retVal == null ? null : sanitizeConfigSetting(retVal);
+    }
 
-	    String result = null;
-	    try {
-	        result = out.toString("UTF-8");
-	    }
-	    catch (UnsupportedEncodingException e) {
-	        logger.error(e.getMessage());
-	    }
-	    return result;
-	}
+    public static List<String> getFieldValues(Record record, String fieldSpecs) {
+        List<String> results = new ArrayList<String>();
+        String[] specs = fieldSpecs.split(":");
+        for (String spec : specs) {
+            if (spec.length() == 4) {
+                String tag = spec.substring(0, 3);
+                char code = spec.charAt(3);
+                List<VariableField> fields = record.getVariableFields(tag);
+                for (VariableField f : fields) {
+                    DataField field = (DataField) f;
+                    Subfield subfield = field.getSubfield(code);
+                    if (subfield != null) {
+                        results.add(subfield.getData().trim());
+                    }
+                }
+            }
+        }
+        return results;
+    }
+
+    public static String getFirstFieldValue(Record record, String fieldSpecs) {
+        List<String> values = getFieldValues(record, fieldSpecs);
+        return values.isEmpty() ? null : values.get(0);
+    }
+
+    public static String getRecordId(Record record, String source) {
+        String id = null;
+        if ("sirsi".equals(source)) {
+            List<String> vals = getFieldValues(record, "035a");
+            for (String val : vals) {
+                if (val.startsWith(sirsiCatkeyPrefix)) {
+                    id = val.substring(sirsiCatkeyPrefix.length());
+                    break;
+                }
+            }
+        } else if ("sfx".equals(source)) {
+            id = getFirstFieldValue(record, "090a");
+        } else if ("muler".equals(source)) {
+            id = getFirstFieldValue(record, "035a");
+        }
+        return id;
+    }
+
+    public static String writeRaw(Record record) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        MarcWriter writer = new MarcStreamWriter(out, "UTF-8", true);
+        writer.write(record);
+        writer.close();
+
+        String result = null;
+        try {
+            result = out.toString("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            logger.error(e.getMessage());
+        }
+        return result;
+    }
 }
