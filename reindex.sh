766486ecd87b10bbb74ec0cbad8d99bc0eff52b8
Tuan Nguyen <amtuannguyen@gmail.com>
2014-10-06 11:40:32 -0400

    use VUFIND_HOME instead of hard coded /usr/local/vufind

--- reindex.sh
+++ reindex.sh
@@ -16,18 +16,18 @@ java $INDEX_OPTIONS -jar import/YorkIndexer.jar
 [ -f /tmp/muler.mrc ] && curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>data_source_str:MULER</query></delete>' 1>/dev/null
 
 # import SFX journals marcxml
-[ -f /tmp/sfx-journals.xml ] && /usr/local/vufind/import-sfx.sh /tmp/sfx-journals.xml &>/tmp/import-sfx-journals.log &
+[ -f /tmp/sfx-journals.xml ] &&  ${VUFIND_HOME}/import-sfx.sh /tmp/sfx-journals.xml &>/tmp/import-sfx-journals.log &
 
 # import MULER marc 
-[ -f /tmp/muler.mrc ] && /usr/local/vufind/import-muler.sh /tmp/muler.mrc &>/tmp/import-muler.log &
+[ -f /tmp/muler.mrc ] &&  ${VUFIND_HOME}/import-muler.sh /tmp/muler.mrc &>/tmp/import-muler.log &
 
 wait
 
 # import catalog marc files
-[ -f /tmp/0catalog.mrc ] && /usr/local/vufind/import-marc.sh /tmp/0catalog.mrc &>/tmp/import-catalog0.log &
-[ -f /tmp/1catalog.mrc ] && /usr/local/vufind/import-marc.sh /tmp/1catalog.mrc &>/tmp/import-catalog1.log &
-[ -f /tmp/2catalog.mrc ] && /usr/local/vufind/import-marc.sh /tmp/2catalog.mrc &>/tmp/import-catalog2.log &
-[ -f /tmp/3catalog.mrc ] && /usr/local/vufind/import-marc.sh /tmp/3catalog.mrc &>/tmp/import-catalog3.log &
+[ -f /tmp/0catalog.mrc ] &&  ${VUFIND_HOME}/import-marc.sh /tmp/0catalog.mrc &>/tmp/import-catalog0.log &
+[ -f /tmp/1catalog.mrc ] &&  ${VUFIND_HOME}/import-marc.sh /tmp/1catalog.mrc &>/tmp/import-catalog1.log &
+[ -f /tmp/2catalog.mrc ] &&  ${VUFIND_HOME}/import-marc.sh /tmp/2catalog.mrc &>/tmp/import-catalog2.log &
+[ -f /tmp/3catalog.mrc ] &&  ${VUFIND_HOME}/import-marc.sh /tmp/3catalog.mrc &>/tmp/import-catalog3.log &
 
 wait
 
