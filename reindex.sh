06bc778e4daf8df8203c7c7609a6c727bae03d15
Tuan Nguyen <amtuannguyen@gmail.com>
2014-10-07 12:58:04 -0400

    Make sure MASTER env is set in reindex.sh

--- reindex.sh
+++ reindex.sh
@@ -9,11 +9,17 @@ fi
   
 cd $VUFIND_HOME || exit
 
+if [ -z "$MASTER" ]; then
+  echo "MASTER environment variable not set. Please set it to the URL of the master solr biblio index."
+  echo "Example: export MASTER=http://localhost:8080/solr/biblio"
+  exit 
+fi
+
 # run preprocessing
 java $INDEX_OPTIONS -jar import/YorkIndexer.jar
 
-[ -f /tmp/sfx-journals.xml ] && curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>data_source_str:SFX</query></delete>' 1>/dev/null
-[ -f /tmp/muler.mrc ] && curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>data_source_str:MULER</query></delete>' 1>/dev/null
+[ -f /tmp/sfx-journals.xml ] && curl -s -S ${MASTER}/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>data_source_str:SFX</query></delete>' 1>/dev/null
+[ -f /tmp/muler.mrc ] && curl -s -S ${MASTER}/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>data_source_str:MULER</query></delete>' 1>/dev/null
 
 # import SFX journals marcxml
 [ -f /tmp/sfx-journals.xml ] &&  ${VUFIND_HOME}/import-sfx.sh /tmp/sfx-journals.xml &>/tmp/import-sfx-journals.log &
@@ -34,7 +40,7 @@ wait
 # TODO check log for errors and rollback if necessary
 
 # cleanup and commit
-curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>format:Delete</query></delete>' 1>/dev/null
-curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>title:"**REQUIRED FIELD**"</query></delete>' 1>/dev/null
-curl -s -S http://localhost:$JETTY_PORT/solr/$SOLRCORE/update/ -H "Content-Type: text/xml" --data-binary '<commit/>' 1>/dev/null
+curl -s -S ${MASTER}/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>format:Delete</query></delete>' 1>/dev/null
+curl -s -S ${MASTER}/update/ -H "Content-Type: text/xml" --data-binary '<delete><query>title:"**REQUIRED FIELD**"</query></delete>' 1>/dev/null
+curl -s -S ${MASTER}/update/ -H "Content-Type: text/xml" --data-binary '<commit/>' 1>/dev/null
 
