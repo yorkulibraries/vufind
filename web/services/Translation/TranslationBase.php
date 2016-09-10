6d9d27c98bb6ab1e3d40e0308f14d11a5f726e2a
Tuan Nguyen <amtuannguyen@gmail.com>
2016-09-10 16:08:30 -0400

    add authn

--- web/services/Translation/TranslationBase.php
+++ web/services/Translation/TranslationBase.php
@@ -9,6 +9,10 @@ class TranslationBase extends Action
         global $configArray;
         global $interface;
         
+        if (!UserAccount::isLoggedIn()) {
+            $this->redirectToLogin();
+        }
+        
         $interface->assign('enabledLanguages', $configArray['Languages']);
         $interface->assign('enabledLanguageCodes', array_keys($configArray['Languages']));
     }
@@ -17,6 +21,14 @@ class TranslationBase extends Action
     {
         global $configArray;
         header('Location: ' . $configArray['Site']['path'] . '/Translation/Home');
+        exit;
+    }
+    
+    protected function redirectToLogin()
+    {
+        global $configArray;
+        global $action;
+        header('Location: ' . $configArray['Site']['path'] . '/MyResearch/Home?followup=1&followupModule=Translation&followupAction=' . $action);
         exit;
     }
 }
