ab51085542270c6ff57b28dc895e50788c509d58
Tuan Nguyen <amtuannguyen@gmail.com>
2016-09-12 12:35:22 -0400

    update existing keys when populting table

--- web/services/Translation/Populate.php
+++ web/services/Translation/Populate.php
@@ -23,20 +23,28 @@ class Populate extends TranslationBase
     
     private function loadFromFile($langCode)
     {
+        global $user;
+        
         $translator = new I18N_Translator('lang', $langCode, false, false);
         foreach ($translator->words as $key => $value) {
             $translation = new Translation();
             $translation->key = $key;
             $translation->lang = $langCode;
-            if (!$translation->find()) {
-                $translation->value = $value;
-                $translation->verified = 1;
-                // if language is not 'en' and key and value are the same the we need to flag it 
-                if ($translation->lang != 'en' && $translation->key == $translation->value) {
-                    $translation->verified = 0;
-                }
+            $exists = $translation->find(true);
+            
+            $translation->value = $value;
+            $translation->verified = 1;
+            // if language is not 'en' and key and value are the same the we need to flag it 
+            if ($translation->lang != 'en' && $translation->key == $translation->value) {
+                $translation->verified = 0;
+            }
+            $translation->last_modified_by = $user->id;
+            
+            if ($exists)
                 $translation->insert();
-            }    
+            } else {
+                $translation->update();
+            }
         }
     }
 }
