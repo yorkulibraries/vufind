9d14a293e74ab32c08c7403b634f3419a4754e16
Tuan Nguyen <amtuannguyen@gmail.com>
2016-10-20 14:44:39 -0400

    rename class Generator to CoverGenerator becase we can't use class name Generator after php5.5

--- web/generator.php
+++ web/generator.php
@@ -35,7 +35,7 @@
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     http://vufind.org/wiki/use_of_external_content Wiki
  */
-class Generator
+class CoverGenerator
 {
     /**
      * Style configuration
